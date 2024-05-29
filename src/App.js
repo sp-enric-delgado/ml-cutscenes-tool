c428b3b590dd8d2b4a0b3cca0a1086ae0b1086a4
Abert Boada <albert.boada@socialpoint.es>
2024-05-29 14:22:05 +0200

    Main navigation skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import SceneSection from "./Sections/SceneSection/SceneSection";
+import StepsSection from "./Sections/StepsSection/StepsSection";
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PAGE_SCENE = 1;
+const PAGE_STEPS = 2;
+
+export default function App() {
+    const [currentPage, setCurrentPage] = useState(PAGE_SCENE);
+
+    function goToSteps() {
+        setCurrentPage(PAGE_STEPS);
+    }
+
+    function goToScene() {
+        setCurrentPage(PAGE_SCENE);
+    }
+
+    return (
+        <div>
+            <div className={currentPage !== PAGE_SCENE ? "hidden" : ""}>
+                <SceneSection OnNextClicked={goToSteps}/>
+            </div>
+            <div className={currentPage !== PAGE_STEPS ? "hidden" : ""}>
+                <StepsSection OnPreviousClicked={goToScene} />
+            </div>
+        </div>
+    );
 }
 
-export default App;
