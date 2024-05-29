d8bce60ac7f08a62b36b7ec4339b662803de6616
Abert Boada <albert.boada@socialpoint.es>
2024-05-29 14:26:35 +0200

    Fixes navigation

--- src/Sections/SceneSection/SceneSection.js
+++ src/Sections/SceneSection/SceneSection.js
@@ -1,4 +1,4 @@
-export default function SceneSection(OnNextClicked) {
+export default function SceneSection({OnNextClicked}) {
     return (
         <div>
             <h1>Scene</h1>
--- src/Sections/StepsSection/StepsSection.js
+++ src/Sections/StepsSection/StepsSection.js
@@ -1,8 +1,8 @@
-export default function StepsSection(OnPreviousClicked) {
+export default function StepsSection({OnPreviousClicked}) {
     return (
         <div>
             <h1>Steps</h1>
-            <button onClick={() => OnPreviousClicked()}>Previous</button>
+            <button onClick={OnPreviousClicked}>Previous</button>
         </div>
     );
 }
