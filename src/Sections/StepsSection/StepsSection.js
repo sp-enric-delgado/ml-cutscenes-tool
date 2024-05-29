fda6337e81d1c84a180ecf8871a372bb8a31a371
Enric Delgado Claramunt <enric.delgado@socialpoint.es>
2024-05-29 15:32:19 +0200

    Fabric Canvas Created

--- src/Sections/StepsSection/StepsSection.js
+++ src/Sections/StepsSection/StepsSection.js
@@ -1,8 +1,45 @@
+import {useRef, useState, useEffect} from "react";
+import { fabric } from 'fabric';
+
+import "./styles/StepsSection.css"
+
+const CANVAS_WIDTH = 960;
+const CANVAS_HEIGHT = 540;
+
 export default function StepsSection({OnPreviousClicked}) {
+
+    const [canvas, setCanvas] = useState(null);
+    const canvasRef = useRef();
+
+    useEffect(() => {
+        var cnv = new fabric.Canvas(canvasRef.current);
+        cnv.setDimensions({width: CANVAS_WIDTH, height: CANVAS_HEIGHT});
+
+        setCanvas(cnv);
+    }, []);
+
     return (
-        <div>
-            <h1>Steps</h1>
-            <button onClick={OnPreviousClicked}>Previous</button>
+        <div className="section">
+            <div>
+                <button onClick={OnPreviousClicked}>Previous</button>
+            </div>
+
+            <div className="body">
+                <div className="canvasSection">
+                    <h1 className="title">Canvas</h1>
+                    <div className="canvasDiv">
+                        <canvas ref={canvasRef} className="canvas"/>
+                    </div>
+                </div>
+
+                <div className="stepsSection">
+                    <h1 className="title">Steps</h1>
+                    <div className="steps">
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
